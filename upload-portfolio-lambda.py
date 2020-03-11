66f1d9aadaeeb0e71ec614335cbf315cca72840c
felsenkraxler <felsenkraxler@gmail.com>
2020-03-11 17:28:46 +0100

    erweitern der lambdafunktion

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,25 +4,31 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-
-    #session = boto3.Session(profile_name='eiskraxlerportfolio')
-    s3bucket = boto3.resource('s3')
-    
-    portfolio_bucket=s3bucket.Bucket('portfolio.stefanbauer.solutions')
-    build_bucket=s3bucket.Bucket('portfoliobuild.stefanbauer.solutions')
-    
-    
-    portfolio_zip=io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    
-    
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj=myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
+    sns=boto3.resource('sns')
+    topic=sns.Topic('arn:aws:sns:us-east-1:524339592450:deployPorfolioTopic')
+    try:
+        #session = boto3.Session(profile_name='eiskraxlerportfolio')
+        s3bucket = boto3.resource('s3')
+        
+        portfolio_bucket=s3bucket.Bucket('portfolio.stefanbauer.solutions')
+        build_bucket=s3bucket.Bucket('portfoliobuild.stefanbauer.solutions')
+        
+        
+        portfolio_zip=io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        
+        
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj=myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        topic.publish(Subject="Porfolio Deployment",Message="Portfolio deployed successfully")
+    except:
+        topic.publish(Subject="Porfolio Deployment Faild",Message="Portfolio was NOT deployed successfully!!")
+        raise
     return 'Hello from Lambda'
